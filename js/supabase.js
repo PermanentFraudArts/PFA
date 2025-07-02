60342a4a4e648d87798c5294350e4667ef688ba2
PermanentFraudArts <permanent.fraud.arts@gmail.com>
2025-07-03 00:21:49 +0900

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
 const supabase = createClient(
-  'https://ssefoacawrecfxtqbld.supabase.co',
+  'https://ssefoarcawrecfxtqbld.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzZWZvYXJjYXdyZWNmeHRxYmxkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE0NDg2MjgsImV4cCI6MjA2NzAyNDYyOH0.GX-r5l7F6Zd1pI8UF0ARX6wDFPNrV9lZJb2Jr7AUp_o'
 )
 
